ad1fad525f6f2968c828824d253a6a0751e63ac6
Migue <miguelangelneumann@gmail.com>
2024-11-13 23:13:30 -0300

    eliminando imagenes y editado del manual

--- database/conexion.php
+++ database/conexion.php
@@ -1,6 +1,6 @@
 <?php
 //$conexion = new mysqli("fra1.clusters.zeabur.com", "root", "3SRdH0ge6JQz21tCG7mXbkVq4O9ov85u", "pixfood", 32005);
-$conexion = new mysqli("localhost", "root", "", "pixfood");
+$conexion = new mysqli("localhost", "root", "1234", "pixfood");
 
 
 // Verificar la conexión
